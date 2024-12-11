851ac879a841b25d7ba61c5107a2bfdf4c21e7ef
Ramasamy Kandasamy <kramasamy90@gmail.com>
2024-12-11 20:14:46 +0530

    Conditionally return intermediate models.

--- utils/trainer.py
+++ utils/trainer.py
@@ -9,9 +9,6 @@ from torch.utils.data import DataLoader
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from optimizer_factory import OptimizerFactory
 
-# from typing import 
-
-# Import training parameters.
 
 class Trainer:
     loss_fns = {
@@ -27,25 +24,34 @@ class Trainer:
         self.train_dataset = train_dataset
 
     
-    def train(self, inner_progress_bar = False, outer_progress_bar = True, 
-              print_loss = False):
+    def train(self,
+              inner_progress_bar = False,
+              outer_progress_bar = True, 
+              print_loss = False,
+              return_intermediate_models = False):
         self.model.to(self.config['device'])
 
         # Get optimizer.
         optimizer_factory = OptimizerFactory(self.config['optimizer'])
         optimizer = optimizer_factory.get_optimizer(self.model.parameters())
+
         # Loss function.
-
         criterion = self.loss_fns[self.config['loss_fn']]().to(self.config['device'])
-
+        # Dataset.
         train_loader = DataLoader(self.train_dataset,
                                     batch_size=self.config['batch_size'], shuffle=True)
+        
         if inner_progress_bar:
             train_loader = tqdm(train_loader)
         if outer_progress_bar:
             epochs_iterator = tqdm(range(self.config['epochs']))
         else:
             epochs_iterator = range(self.config['epochs'])
+        
+        if return_intermediate_models:
+            intermediate_models = []
+        
+        output = {}
 
         self.model.train()
         loss_history = []
@@ -64,7 +70,18 @@ class Trainer:
             if print_loss:
                 print(f"Epoch {epoch+1}/{self.config['epochs']},\
                     Loss: {running_loss/len(train_loader)}")
+            if return_intermediate_models:
+                intermediate_models.append(self.model)
 
             loss_history.append(running_loss / len(train_loader))
-        return {"model": self.model, "loss_history" : loss_history}
+
+        output = {
+            'model' : self.model,
+            'loss_history' : loss_history,
+        }
+
+        if return_intermediate_models:
+            output['intermediate_models'] = intermediate_models
+    
+        return output
 
