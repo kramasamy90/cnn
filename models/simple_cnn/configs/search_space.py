87e0a5e2d483155b4139b27db6558727a8bee6e1
Ramasamy Kandasamy <kramasamy90@gmail.com>
2024-12-14 18:23:31 +0530

    Tune for hyperparameters.

--- models/simple_cnn/configs/search_space.py
+++ models/simple_cnn/configs/search_space.py
@@ -5,16 +5,16 @@ search_space = {
         {
             'name' : 'sgd',
             'params' : {
-                'momentum' : hp.uniform('sgd_momentum', 0.001, 0.01),
                 'lr': hp.uniform('sgd_lr', 0.001, 0.1),
-                'weight_decay': hp.uniform('sgd_weight_decay', 0.5, 0.99)
+                'weight_decay': hp.uniform('sgd_weight_decay', 1e-5, 1e-2),
+                'momentum' : hp.uniform('sgd_momentum', 0.001, 0.01)
             }
         },
         {
             'name' : 'adam',
             'params' : {
                 'lr': hp.uniform('adam_lr', 0.001, 0.1),
-                'weight_decay': hp.uniform('adam_weight_decay', 0.5, 0.99),
+                'weight_decay': hp.uniform('adam_weight_decay', 1e-5, 1e-2),
                 'beta1' : hp.uniform('adam_beta1', 0.9, 0.99),
                 'beta2' : hp.uniform('adam_beta2', 0.99, 0.999)
             }
@@ -23,7 +23,7 @@ search_space = {
             'name' : 'rmsprop',
             'params' : {
                 'lr': hp.uniform('rmsprop_lr', 0.001, 0.1),
-                'weight_decay': hp.uniform('rmsprop_weight_decay', 0.5, 0.99),
+                'weight_decay': hp.uniform('rmsprop_weight_decay', 1e-5, 1e-2),
                 'momentum' : hp.uniform('rmsprop_momentum', 0.001, 0.01),
                 'alpha' : hp.uniform('rmsprop_alpha', 0.9, 0.99)
             }
@@ -31,5 +31,6 @@ search_space = {
     ]),  
     'loss_fn' : 'ce_loss',
     'batch_size' : hp.choice('batch_size', [16, 32, 64, 128]),
-    'epochs' : hp.choice('epochs', [5])
+    'epochs' : hp.choice('epochs', [5]),
+    'device' : 'cuda'
 }
