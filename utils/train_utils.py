523c677e2d96ab0bf4ec697ca6f737052ada651e
Ramasamy Kandasamy <kramasamy90@gmail.com>
2024-12-15 13:01:36 +0530

    Add functions for initialization and gradient norm.

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -8,6 +8,35 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
+
+## Utility functions for monitoring.
+def get_grad_norm(model):
+    total_norm = 0.0
+    for p in model.parameters():
+        if p.grad is not None:
+            param_norm = p.grad.data.norm(2)  # Compute L2 norm
+            total_norm += param_norm.item() ** 2
+    total_norm = total_norm ** 0.5  # Final L2 norm for the entire model
+    return total_norm
+
+
+## Initialization
+def he_init(module):
+    if isinstance(module, nn.Linear) or isinstance(module, nn.Conv2d):
+        nn.init.kaiming_normal_(module.weight, mode='fan_in',
+                                 nonlinearity='relu')
+        if module.bias is not None:
+            nn.init.constant_(module.bias, 0)
+
+
+def xavier_init(module):
+    if isinstance(module, nn.Linear) or isinstance(module, nn.Conv2d):
+        nn.init.xavier_normal_(module.weight, mode='fan_in',
+                                 nonlinearity='relu')
+        if module.bias is not None:
+            nn.init.constant_(module.bias, 0)
+
+
 class OptimizerFactory:
     optimizers = {
         'sgd'       : torch.optim.SGD,
@@ -41,7 +70,9 @@ class Trainer:
     def train(self,
               loss_fn = None,
               progress_bar = True, 
+              inner_progress_bar = False, 
               print_loss = False,
+              print_grad_norm = False,
               return_intermediate_models = False):
 
         device = self.config.get('device', 'cpu')
@@ -60,6 +91,9 @@ class Trainer:
         # Dataset.
         train_loader = DataLoader(self.train_dataset,
                         batch_size=self.config['batch_size'], shuffle=True)
+        
+        if inner_progress_bar:
+            train_loader = tqdm(train_loader)
         
         if progress_bar:
             epochs_iterator = tqdm(range(self.config['epochs']))
@@ -93,6 +127,11 @@ class Trainer:
                 min_loss = running_loss
                 best_epoch = epoch
                 best_model = deepcopy(self.model)
+            
+            if print_grad_norm:
+                grad_norm = get_grad_norm(self.model)
+                print(f"Epoch {epoch+1}/{self.config['epochs']},\
+                    Gradient norm: {grad_norm}")
 
             if print_loss:
                 print(f"Epoch {epoch+1}/{self.config['epochs']},\
